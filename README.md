710d4ba128119d5e09158a2ea6fd935c53bd625c
Aidan Chadha <aidanchadha@Aidans-MacBook-Pro.local>
2026-07-10 18:20:39 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Create a CLI tool that parses a file(s) from one specific extension type
+and converts it into another. I have to do this all the time for uploading images,
+and changing from HEIC on Apple to JPG or PNG I always go to another website to do it - might as well
+create my own CLI tool to do it in house.
---
+++ src/main.rs
@@ -0,0 +1,59 @@
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(Parser, Debug)]
+struct Cli {
+    #[arg(required = true, value_name = "FILE")]
+    files: Vec<PathBuf>,
+
+    #[arg(required = true, short, long, value_name = "CONVERSION_TYPE")]
+    to: String,
+}
+
+#[derive(Debug)]
+struct Files {
+    files: Vec<File>,
+    completed: bool,
+    to_extension: String,
+}
+
+#[derive(Debug, Default)]
+struct File {
+    path: std::path::PathBuf,
+    completed: bool,
+    extension: String,
+}
+
+fn main() {
+    let args = Cli::parse();
+
+    let mut files = Files {
+        files: Vec::<File>::new(),
+        completed: false,
+        to_extension: args.to,
+    };
+
+    for file in args.files.iter() {
+        let mut extension: String = String::new();
+        if let Some(ext) = std::path::Path::new(file).extension() {
+            if let Some(ext_str) = ext.to_str() {
+                extension = String::from(ext_str);
+            }
+        }
+        let curr_file = File {
+            path: std::path::PathBuf::from(file),
+            completed: false,
+            extension: extension,
+        };
+        if !curr_file.path.exists() {
+            eprintln!(
+                "Path: {:?} does not existing within the CWD.",
+                curr_file.path
+            );
+            return;
+        }
+        files.files.push(curr_file);
+    }
+    println!("All collected files: {:?}", files);
+    return;
+}
